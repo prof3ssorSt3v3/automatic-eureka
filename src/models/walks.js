ade9b3397fce54d76a4f1fbcbd64f119a27224c8
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2026-01-02 13:07:42 -0500

    update data dates

--- src/models/walks.js
+++ src/models/walks.js
@@ -1,14 +1,14 @@
 const walks = [
   {
     id: 1,
-    startTime: new Date('2024-01-01T14:00'),
-    endTime: new Date('2024-01-01T14:30'),
+    startTime: new Date('2026-01-02T14:00'),
+    endTime: new Date('2026-01-02T14:30'),
     city: 'Ottawa',
   },
   {
     id: 2,
-    startTime: new Date('2024-01-03T15:00'),
-    endTime: new Date('2024-01-03T15:45'),
+    startTime: new Date('2026-01-03T15:00'),
+    endTime: new Date('2026-01-03T15:45'),
     city: 'Ottawa',
   },
 ];
